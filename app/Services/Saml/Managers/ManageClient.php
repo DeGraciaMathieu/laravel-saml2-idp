74e0aea6be6b13c7c7831079a8ae894e8ad5468e
DeGraciaMathieu <contact@degracia-mathieu.fr>
2018-02-22 13:19:04 -0800

    psr style

--- app/Services/Saml/Managers/ManageClient.php
+++ app/Services/Saml/Managers/ManageClient.php
@@ -8,32 +8,32 @@ use App\Exceptions\MissingIssuerException;
 
 class ManageClient {
 
-	/**
-	 * Retrieve client by the issuer of a SAML Message
-	 * @param  \LightSaml\Model\Protocol\SamlMessage $message
-     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
-     * @throws \App\Exceptions\MissingIssuerException
-	 * @return \App\Client
-	 */
-	public function getByMessageIssuer(SamlMessage $message)
-	{
-		$entityId = $message->getIssuer()->getValue();
+    /**
+    * Retrieve client by the issuer of a SAML Message
+    * @param  \LightSaml\Model\Protocol\SamlMessage $message
+    * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+    * @throws \App\Exceptions\MissingIssuerException
+    * @return \App\Client
+    */
+    public function getByMessageIssuer(SamlMessage $message)
+    {
+        $entityId = $message->getIssuer()->getValue();
 
-		if (! $entityId) {
-			throw new MissingIssuerException();
-		}
+        if (! $entityId) {
+            throw new MissingIssuerException();
+        }
 
-		return $this->getByEntityId($entityId);
-	}
+        return $this->getByEntityId($entityId);
+    }
 
-	/**
-	 * Retrieve client by the entityId
-	 * @param  string $entityId 
-     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
-	 * @return \App\Client
-	 */
-	public function getByEntityId($entityId)
-	{
-		return Client::where('entity_id', $entityId)->firstOrFail();
-	}
+    /**
+    * Retrieve client by the entityId
+    * @param  string $entityId 
+    * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+    * @return \App\Client
+    */
+    public function getByEntityId($entityId)
+    {
+        return Client::where('entity_id', $entityId)->firstOrFail();
+    }
 }
