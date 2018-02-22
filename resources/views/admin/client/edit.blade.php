9c784d60d0b4d86a968905b5a233053f15bea7ca
DeGraciaMathieu <contact@degracia-mathieu.fr>
2018-02-22 13:03:33 -0800

    Amélioration page client.edit

--- resources/views/admin/client/edit.blade.php
+++ resources/views/admin/client/edit.blade.php
@@ -14,34 +14,37 @@
               <li><a href="{{ route('client.index') }}">Clients</a></li>
               <li class="active">{{ $client->entity_id }}</li>
             </ol>
+            @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <strong>Whoops!</strong> There were some problems with your input.
+                </div>
+            @endif
             <div class="panel panel-default">
                 <div class="panel-heading">Tableau de bord</div>
                 <div class="panel-body">
-                @if (count($errors) > 0)
-                    <div class="alert alert-danger">
-                        <strong>Whoops!</strong> There were some problems with your input.<br><br>
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
-                <?php
-                ?>
                 <form method='POST' action="{{ route('client.update', $client->id) }}">
                     {!! method_field('patch') !!}
                     <div class="row">
                         <div class="col-xs-12 col-sm-12 col-md-12">
-                            <div class="form-group">
+                            <div class="form-group {{ $errors->has('entity_id') ? 'has-error' : '' }}">
                                 <strong>entity_id:</strong>
                                 <input type="input" name="entity_id" class='form-control' value='{{$client->entity_id}}'>
+                                @if ($errors->has('entity_id'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('entity_id') }}</strong>
+                                    </span>
+                                @endif
                             </div>
                         </div>
                         <div class="col-xs-12 col-sm-12 col-md-12">
-                            <div class="form-group">
+                            <div class="form-group {{ $errors->has('endpoint') ? 'has-error' : '' }}">
                                 <strong>endpoint:</strong>
                                 <input type="input" name="endpoint" class='form-control' value='{{$client->endpoint}}'>
+                                @if ($errors->has('endpoint'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('endpoint') }}</strong>
+                                    </span>
+                                @endif                                
                             </div>
                         </div>                                              
                         <div class="col-xs-12 col-sm-12 col-md-12 text-center">
