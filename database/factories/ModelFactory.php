c1b7a5371df0ebfae6c461bbb65979997dc9a3b7
DeGraciaMathieu <contact@degracia-mathieu.fr>
2018-02-22 14:34:06 -0800

    Ajout uuid dans database

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -16,6 +16,7 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
+        'uuid' => $faker->uuid,
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
