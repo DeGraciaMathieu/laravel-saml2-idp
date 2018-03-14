27c1cc65933226e40f8c0ec07a4f130abfbddb40
DeGraciaMathieu <contact@degracia-mathieu.fr>
2018-03-14 05:23:13 -0700

    Ajout d'un message sur le endpoint du client sur la page de login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use SamlService;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
@@ -36,4 +37,21 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    /**
+     * Show the application's login form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showLoginForm()
+    {
+        $endpoint = null;
+
+        if (SamlService::samlRequestIsPending()) {
+
+            $endpoint = SamlService::getPendingSamlRequestEndpoint();
+        }
+
+        return view('auth.login', ['endpoint' => $endpoint]);
+    }    
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,13 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
+
+            @if ($endpoint)
+            <div class="alert alert-info">
+              <strong>Info ! </strong> Après connexion vous serez redirigé vers : {!!$endpoint!!}
+            </div>
+            @endif
+            
             <div class="panel panel-default">
                 <div class="panel-heading">Login</div>
 
