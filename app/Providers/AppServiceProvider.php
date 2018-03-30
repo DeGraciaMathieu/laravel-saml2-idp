f554117782569d81d9c780524038f62310e04c82
DeGraciaMathieu <contact@degracia-mathieu.fr>
2018-03-30 12:26:45 -0700

    Nettoyage AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,14 +2,9 @@
 
 namespace App\Providers;
 
-use App\Services\Saml\SamlService;
+use Auth;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
-use App\Services\Saml\Managers\ManageClient;
-use App\Services\Saml\Managers\ManageRequest;
-use App\Services\Saml\Managers\ManageMessage;
-use App\Services\Saml\Managers\ManageResponse;
-use App\Services\Saml\Managers\ManageSignature;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -30,8 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(ManageSignature::class, function ($app) {
-            return new ManageSignature();
-        });
+        //
     }
 }
